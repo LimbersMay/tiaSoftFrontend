26f34a94a383a6b3d20fbbe8cdb553aec41f6f7b
LimbersMay <limbermay1@gmail.com>
2024-08-12 21:24:27 -0600

    Componentes de autenticación creados

---
+++ src/app/auth/pages/new-account-page/new-account-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-new-account-page',
+  templateUrl: './new-account-page.component.html',
+  styles: []
+})
+export class NewAccountPageComponent {
+
+}
