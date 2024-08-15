0bd736ff05138539e6936e6d192183883245562c
LimbersMay <limbermay1@gmail.com>
2024-08-14 20:48:44 -0600

    Importación del commonModule eliminado en los módulos de primeng y shared

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {InputTextModule} from "primeng/inputtext";
 import {ButtonModule} from "primeng/button";
 
@@ -7,9 +6,7 @@ import {ButtonModule} from "primeng/button";
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
+  imports: [],
   exports: [
     InputTextModule,
     ButtonModule
