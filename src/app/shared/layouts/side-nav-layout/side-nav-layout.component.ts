b0f2f174150ec9de2aa7bb7530446d7b1d440ab1
LimbersMay <limbermay1@gmail.com>
2024-08-18 14:36:11 -0600

    Componente de SideNav compartido agregado al módulo de shared

---
+++ src/app/shared/layouts/side-nav-layout/side-nav-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-side-nav-layout',
+  templateUrl: './side-nav-layout.component.html',
+  styles: ``
+})
+export class SideNavLayoutComponent {
+
+}
