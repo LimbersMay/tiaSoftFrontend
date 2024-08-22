81744e86366ba5147b1c3d82e76a37034bb4cd9c
LimbersMay <limbermay1@gmail.com>
2024-08-21 22:07:25 -0600

    Botón para mostrar y ocultar el sidenav implementado en el layout

--- src/app/shared/layouts/side-nav-layout/side-nav-layout.component.ts
+++ src/app/shared/layouts/side-nav-layout/side-nav-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../../auth/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'shared-side-nav-layout',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class SideNavLayoutComponent {
 
+  public showNavBar = false;
+
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+  ) {}
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: () => this.router.navigate(['/auth/login']),
+      error: () => this.router.navigate(['/auth/login'])
+    });
+  }
+
+  toggleNavBar() {
+    this.showNavBar = !this.showNavBar;
+  }
 }
