31c78b4332f0f280b476b0770c758fc784a1357e
LimbersMay <limbermay1@gmail.com>
2024-08-21 22:09:32 -0600

    Módulo de MessageService agregado en el módulo principal para usar el toast de primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MessageService} from "primeng/api";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
