b6cce6580956d0e8ca9a28e1817af7b95b487176
LimbersMay <limbermay1@gmail.com>
2024-08-21 22:06:52 -0600

    Módulo NgClass agregado al módulo de shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,19 +3,21 @@ import {SideNavLayoutComponent} from "./layouts/side-nav-layout/side-nav-layout.
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {PrimeNgModule} from "../prime-ng/prime-ng.module";
 import {StyleClassModule} from "primeng/styleclass";
+import {NgClass} from "@angular/common";
 
 
 @NgModule({
   declarations: [
     SideNavLayoutComponent
   ],
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    PrimeNgModule,
-    StyleClassModule,
-    RouterLinkActive
-  ],
+    imports: [
+        RouterOutlet,
+        RouterLink,
+        PrimeNgModule,
+        StyleClassModule,
+        RouterLinkActive,
+        NgClass
+    ],
   exports: [
     SideNavLayoutComponent
   ]
