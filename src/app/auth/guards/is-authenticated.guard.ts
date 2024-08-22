360cf64f162d00468f53a82a251105ca771b8f07
LimbersMay <limbermay1@gmail.com>
2024-08-21 22:17:09 -0600

    Se guardó la url a la que el usuario intentó acceder al localStorage para redirigirlo hacia él al iniciar sesión

--- src/app/auth/guards/is-authenticated.guard.ts
+++ src/app/auth/guards/is-authenticated.guard.ts
@@ -3,10 +3,13 @@ import {inject} from "@angular/core";
 import {AuthService} from "../services/auth.service";
 import {AuthStatus} from "../interfaces/auth-status.enum";
 
-export const isAuthenticatedGuard: CanActivateFn = () => {
+export const isAuthenticatedGuard: CanActivateFn = (_route, state) => {
 
   const authService = inject(AuthService);
   const router = inject(Router);
+
+  const url = state.url;
+  localStorage.setItem('redirectUrl', url);
 
   if (authService.authStatus === AuthStatus.AUTHENTICATED) {
     return true;
