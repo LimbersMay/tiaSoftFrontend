060c47aa2df79c560f37b3350d55ff75084d139f
LimbersMay <limbermay1@gmail.com>
2024-08-21 22:19:39 -0600

    Configuración básica del ruteo y página principal del dashboard agregado

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainPageComponent} from "./pages/main-page/main-page.component";
+import {SideNavLayoutComponent} from "../shared/layouts/side-nav-layout/side-nav-layout.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: MainPageComponent
+    component: SideNavLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: MainPageComponent
+      },
+      {
+        path: "**",
+        redirectTo: ''
+      }
+    ]
   },
-  {
-    path: '**',
-    redirectTo: ''
-  }
 ];
 
 @NgModule({
