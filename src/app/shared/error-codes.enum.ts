d4c3c9fdf73b60b560d0778545a4f15abd8c1490
LimbersMay <limbermay1@gmail.com>
2024-08-21 22:08:47 -0600

    Se actualizaron los códigos de error

--- src/app/shared/error-codes.enum.ts
+++ src/app/shared/error-codes.enum.ts
@@ -1,4 +1,12 @@
 export enum ErrorCodes {
   AuthErrorIncorrectCredentials = 'AuthErrorIncorrectCredentials',
   AuthErrorEmailAlreadyExists = 'AuthErrorEmailAlreadyExists',
+  AuthErrorNotAuthorized = 'AuthErrorNotAuthorized',
+
+  // USER ERRORS
+  UserNotFound = 'UserNotFound',
+  UserNotUpdated = 'UserNot',
+  UserErrorWhenUpdatingUSer = 'UserErrorWhenUpdatingUSe',
+  UserErrorWhenCreatingUser = 'UserErrorWhenCreatingUser',
+  UserErrorUserNotCreated = 'UserErrorUserNotCreated',
 }
