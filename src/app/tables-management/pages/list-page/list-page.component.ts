ee81d6b1a0cf97a316ff13aeb83b952282d22652
LimbersMay <limbermay1@gmail.com>
2024-09-16 18:21:16 -0600

    Se agregó un menú de opciones en lugar de botones

--- src/app/tables-management/pages/list-page/list-page.component.ts
+++ src/app/tables-management/pages/list-page/list-page.component.ts
@@ -1,13 +1,12 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
-import {MessageService} from "primeng/api";
+import {MenuItem, MessageService} from "primeng/api";
 import {ErrorService} from "../../../shared/services/error.service";
 import {ValidatorsService} from "../../../shared/services/validators.service";
 import {Table} from "../../interfaces/table.interface";
 import {TablesService} from "../../services/tables.service";
 import {Area} from "../../../areas-management/interfaces/area.interface";
 import {AreasService} from "../../../areas-management/services/areas.service";
-import {AuthService} from "../../../auth/services/auth.service";
 import {CreateTableDto} from "../../interfaces/create-table.dto";
 import {TableStatus} from "../../enums/table-status.enum";
 import { TableStatus as ITableStatus } from "../../interfaces/tableStatus.interface";
@@ -25,9 +24,17 @@ export class ListPageComponent implements OnInit {
   public areas: Area[] = [];
   public waiters: User[] = [];
   public tableStatuses: ITableStatus[] = [];
-  public display!: boolean;
 
-  public tableForm = this.fb.group({
+  public display!: boolean;
+  public displayRequestPaymentAuthorization!: boolean;
+  public displayWhereToPrintTicket!: boolean;
+
+  public selectedTable!: Table;
+
+  // p-menu items
+  public items: MenuItem[] | undefined;
+
+  public tableForm = this.fb.nonNullable.group({
     // Hidden fields
     tableId: [''],
     userId: [''],
@@ -36,13 +43,21 @@ export class ListPageComponent implements OnInit {
     name: ['', Validators.required],
     customers: [0],
     areaId: [''],
-  })
+  });
+
+  public authorizationForm = this.fb.nonNullable.group({
+    tableId: [''],
+    tableStatus: this.fb.nonNullable.group({
+      tableStatusId: [''],
+      name: [''],
+      description: ['']
+    }),
+  });
 
   constructor(
     // Domain services
     private readonly tablesService: TablesService,
     private readonly areasService: AreasService,
-    private readonly authService: AuthService,
     private readonly usersService: UsersService,
 
     private readonly messageService: MessageService,
@@ -51,7 +66,31 @@ export class ListPageComponent implements OnInit {
     private readonly validatorsService: ValidatorsService
   ) {}
 
-  public get currentTable() {
+  // KEYBOARD EVENTS on press Key 'N'
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === 'n') {
+      const index = this.tables.findIndex(t => t.tableId === this.authorizationForm.value.tableId);
+      const newTable = structuredClone(this.tables[index]);
+
+      if (index === -1) {
+        this.showDialog();
+        return;
+      }
+
+      newTable.tableStatus.name = TableStatus.Pagado;
+      this.tables[index] = newTable;
+
+      this.closeRequestPaymentAuthorizationDialog();
+      this.messageService.add({severity: 'success', summary: 'Mesa pagada', detail: 'Mesa pagada correctamente'});
+    }
+  }
+
+  public setSelectedTable (table: Table) {
+    this.selectedTable = table;
+  }
+
+  public get currentFormTable() {
     return this.tableForm.value;
   }
 
@@ -61,25 +100,55 @@ export class ListPageComponent implements OnInit {
         return 'info';
       case TableStatus.Pagado:
         return 'success';
+      case TableStatus.PorAutorizar:
+        return 'warning';
+      case TableStatus.Cancelado:
+        return 'danger';
     }
   }
 
   // ------------- DIALOG METHODS -------------
-
-  public showDialog(table?: Table) {
+  public showDialog() {
     this.display = true;
-    this.tableForm.reset(table);
 
-    if (this.authService.currentUser?.userId)
-      this.tableForm.get('userId')?.setValue(this.authService.currentUser?.userId);
+    if (!this.selectedTable) {
+      this.tableForm.reset();
+      return;
+    }
 
-    if (table?.area)
-      this.tableForm.get('areaId')?.setValue(table?.area.areaId);
+    this.tableForm.patchValue({
+      tableId: this.selectedTable.tableId,
+      name: this.selectedTable.name,
+      customers: this.selectedTable.customers,
+      areaId: this.selectedTable.area.areaId,
+    });
   }
 
   public closeDialog(): void {
     this.display = false;
     this.tableForm.reset();
+  }
+
+  public showRequestPaymentAuthorizationDialog(table: Table) {
+    this.displayRequestPaymentAuthorization = true;
+
+    this.authorizationForm.patchValue({
+      tableId: table.tableId,
+      tableStatus: table.tableStatus
+    });
+  }
+
+  public closeRequestPaymentAuthorizationDialog() {
+    this.displayRequestPaymentAuthorization = false;
+    this.tableForm.reset();
+  }
+
+  public showWhereToPrintTicketDialog() {
+    this.displayWhereToPrintTicket = true;
+  }
+
+  public closeWhereToPrintTicketDialog() {
+    this.displayWhereToPrintTicket = false;
   }
 
   // ------------- CRUD METHODS -------------
@@ -90,8 +159,8 @@ export class ListPageComponent implements OnInit {
       return;
     }
 
-    if (!this.currentTable.tableId) {
-      return this.tablesService.createTable(this.currentTable as CreateTableDto).subscribe({
+    if (!this.currentFormTable.tableId) {
+      return this.tablesService.createTable(this.currentFormTable as CreateTableDto).subscribe({
         next: () => {
           this.closeDialog();
           this.messageService.add({severity: 'success', summary: 'Mesa creada', detail: 'Mesa creada correctamente'});
@@ -104,7 +173,7 @@ export class ListPageComponent implements OnInit {
     }
 
     // ------------- UPDATE TABLE -------------
-    return this.tablesService.updateTable(this.currentTable as UpdateTableDto).subscribe({
+    return this.tablesService.updateTable(this.currentFormTable as UpdateTableDto).subscribe({
       next: _ => {
         this.closeDialog();
         this.messageService.add({severity: 'success', summary: 'Mesa actualizada', detail: 'Mesa actualizada correctamente'});
@@ -116,7 +185,66 @@ export class ListPageComponent implements OnInit {
     });
   }
 
+  public sendTableToCashier(table: Table) {
+    this.tablesService.sendTableToCashier(table.tableId).subscribe({
+      next: _ => {
+        this.messageService.add({severity: 'success', summary: 'Cuenta generada', detail: 'Cuenta generada correctamente'});
+      },
+      error: error => {
+        const errorMessage = this.errorService.getErrorMessage(error);
+        this.messageService.add({severity: 'error', summary: 'Error', detail: errorMessage});
+      }
+    });
+  }
+
   public ngOnInit() {
+
+    this.items = [
+      {
+        label: 'Gestión de Mesa',
+        items: [
+          {
+            label: 'Editar Mesa',
+            icon: 'pi pi-pencil',
+            command: () => {
+              this.showDialog();
+            }
+          },
+          {
+            label: 'Visualizar Mesa',
+            icon: 'pi pi-eye',
+          }
+        ]
+      },
+      { separator: true }, // Separador para dividir los grupos
+      {
+        label: 'Acciones de Cobro',
+        items: [
+          {
+            label: 'Enviar a Caja',
+            icon: 'pi pi-send',
+            command: () => {
+              this.sendTableToCashier(this.selectedTable);
+            }
+          },
+          {
+            label: 'Autorizar Cobro',
+            icon: 'pi pi-lock',
+            command: () => {
+              this.showRequestPaymentAuthorizationDialog(this.selectedTable);
+            }
+          },
+          {
+            label: 'Imprimir Cuenta',
+            icon: 'pi pi-print',
+            command: () => {
+              this.showWhereToPrintTicketDialog();
+            }
+          }
+        ]
+      }
+    ];
+
     // ------------- GET TABLES -------------
     this.tablesService.getTables().subscribe({
       next: (tables: Table[]) => this.tables = tables,
@@ -159,11 +287,12 @@ export class ListPageComponent implements OnInit {
         const index = this.tables.findIndex(t => t.tableId === table.tableId);
 
         if (index === -1) {
-          this.tables.push(table);
+          this.tables = [...this.tables, table];
           return;
         }
 
-        this.tables[index] = table;
+       // Update table and refresh the whole list
+        this.tables = this.tables.map(t => t.tableId === table.tableId ? table : t);
       },
       error: (error: any) => {
         const errorMessage = this.errorService.getErrorMessage(error);
